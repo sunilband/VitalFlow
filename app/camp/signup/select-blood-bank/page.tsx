ed4c84c52f3943072bc60ce37876a7a32adb0c79
Sunil Band <sunilband0007@gmail.com>
2024-04-25 23:26:59 +0530

    fix: suspense build issue

--- app/camp/signup/select-blood-bank/page.tsx
+++ app/camp/signup/select-blood-bank/page.tsx
@@ -1,16 +1,18 @@
 import { GridBackgroundDemo } from "@/components/Backgrounds/Grid";
 import SelectBloodBank from "@/components/pages/Camp/CampSignup/SelectBloodBank";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <div>
-      <div className="relative">
-        <GridBackgroundDemo />
-      </div>
-      <SelectBloodBank />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="relative">
+          <GridBackgroundDemo />
+        </div>
+        <SelectBloodBank />
+      </Suspense>
     </div>
   );
 };
--- app/camp/signup/verify/page.tsx
+++ app/camp/signup/verify/page.tsx
@@ -1,16 +1,18 @@
 import { GridBackgroundDemo } from "@/components/Backgrounds/Grid";
 import VerifyCampEmail from "@/components/pages/Camp/CampSignup/VerifyCampEmail/VerifyCampEmail";
-import React from "react";
+import React, { Suspense } from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
     <div>
-      <div className="relative">
-        <GridBackgroundDemo />
-      </div>
-      <VerifyCampEmail />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="relative">
+          <GridBackgroundDemo />
+        </div>
+        <VerifyCampEmail />
+      </Suspense>
     </div>
   );
 };
