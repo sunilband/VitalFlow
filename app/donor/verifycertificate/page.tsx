ab6cfcd11d567c4d4804c22cdf3bab8e11308d24
Sunil Band <sunilband0007@gmail.com>
2024-04-29 22:03:25 +0530

    build : verify certificate

--- app/donor/verifycertificate/page.tsx
+++ app/donor/verifycertificate/page.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import VerifyCertificate from "@/components/pages/Donor/DonorPage/CertificationsManagement.tsx/VerifyCertificate";
 import { GridBackgroundDemo } from "@/components/Backgrounds/Grid";
+import Loader from "@/components/Loader/Loader";
 type Props = {};
 
 const Page = (props: Props) => {
   return (
     <div>
-      <div className="relative">
-        <GridBackgroundDemo />
-      </div>
-      <VerifyCertificate />
+      <Suspense fallback={<Loader />}>
+        <div className="relative">
+          <GridBackgroundDemo />
+        </div>
+        <VerifyCertificate />
+      </Suspense>
     </div>
   );
 };
